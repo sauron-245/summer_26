b39a45a6df81144e4de85372661b2549d54890e3
sauron-245 <miners@carleton.edu>
2026-06-17 15:38:18 -0500

    Added R work to merge logger dfs

---
+++ R work/summer_26/combine_log_df.R
@@ -0,0 +1,37 @@
+# install.packages("tidyverse")
+# install.packages("lubridate") # Comment in if packages have not been installed
+
+library(tidyverse)
+library(lubridate)
+library(readxl)
+
+
+combine_df = function(xl1, xl2){
+  df1 <- read_excel(paste0("wells_merge/", xl1, ".xlsx"),
+                    range = cell_cols("A:G"),
+                    col_names = c("obs", "Date-Time", "Temp", "Conductivity", "SPC", "Salinity", "TDS"),
+                    col_types = c("numeric", "text", "numeric", "numeric", "numeric", "numeric", "numeric")) %>% 
+    slice(-1)  %>% # Removes headers, which are transformed into a row after specifying col_names.
+    mutate(
+      `Date-Time` = parse_date_time(`Date-Time`,
+        orders = c("mdy HMS", "mdy HM", "ymd HMS", "ymd HM"))) 
+  # Date formatting should be addressed on a dataframe-by-dataframe basis
+    
+
+  df2 <- read_excel(paste0("wells_merge/", xl2, ".xlsx"),
+                    range = cell_cols("A:G"),
+                    col_names = c("obs", "Date-Time", "Temp", "Conductivity", "SPC", "Salinity", "TDS"),
+                    col_types = c("numeric", "date", "numeric", "numeric", "numeric", "numeric", "numeric")) %>% 
+    slice(-1)  
+    
+
+  
+  if (ncol(df1) != ncol(df2)) {
+    stop("Column counts do not match — cannot rbind.")
+  }
+  
+  df_new <- rbind(df1, df2)
+
+}
+P1 = combine_df("P1_1", "P1_2") # Adjust names as needed.
+#  May produce warnings -- these are likely due to inconsistencies in column format that we addressed by slicing out the 1st row of each dataframe.
