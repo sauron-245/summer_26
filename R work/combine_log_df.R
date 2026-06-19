83d6cbbd5812bf7f8d404243de34665077c7673c
sauron-245 <miners@carleton.edu>
2026-06-19 11:29:45 -0500

    Continued working on script and spliced creek monitors 1-4 data together

--- R work/combine_log_df.R
+++ R work/combine_log_df.R
@@ -24,17 +24,20 @@ library(readxl)
 
 
 update_well_log = function(site) {
+  cols_to_check = c("Date-Time", "Temperature", "Electrical Conductivity", "Specific Conductivity", "Salinity", "Total Dissolved Solids")
   log_standing <- read_csv(paste0("wells_running/", site, "_current.csv")) # Running total of log data
   
   log_new <- read_excel(paste0("wells_upload/", site, ".xlsx")) %>%  # Most recently downloaded data
-    rename_with(~ trimws(sub("\\(.*$", "", .x))) %>% # Correctly format column names and get rid of 1st column
-    select(-1)
+    rename_with(~ trimws(sub("\\(.*$", "", .x))) %>% # Correctly format column names and get rid of metadata columns
+    select(any_of(cols_to_check))
+  
+  
   
   date_current_last = as_datetime(log_standing[[nrow(log_standing),1]])
   date_new = as_datetime(log_new[[nrow(log_new),1]])
 
   if (date_current_last == date_new) { # Checks if running log data are already up to date
-    stop("Most recent date of running log data matches last date on added data. Have you already merged these dataframes?")
+    stop(paste0("Most recent date of running log data for site ", site, " matches last date on added data. Have you already merged these dataframes?"))
   } 
 
   data_to_add = log_new %>%
@@ -44,10 +47,33 @@ update_well_log = function(site) {
   write_csv(log_standing_updated, paste0("wells_running/", site, "_current.csv")) # Adds new data and overwrites existing. csv
 }
 
-wells_week = c()
+wells_week = c("P1_creek")
 for (well in wells_week) {
   update_well_log(well)
 }
 
+p2_new = read_excel("P4.xlsx")%>%  # Most recently downloaded data
+  rename_with(~ trimws(sub("\\(.*$", "", .x))) %>%   # Correctly format column names and get rid of 1st column
+  select(-1) %>% 
+  rename(Temperature = `Temperature , °C`)
+#   mutate(
+#     Date.Time..CDT. = format(
+#       as.POSIXct(Date.Time..CDT., format = "%m.%d.%Y %H:%M:%S ", tz = "UTC"),
+#       "%Y-%m-%d %H:%M:%S"
+#     )
+  ) %>%
+  rename(`Date-Time` = Date.Time..CDT.) %>%
+  mutate(`Date-Time` = as_datetime(`Date-Time`)) %>%
+  rename(Temperature = Temperature.....C.)
 
-
+p4_running = read.csv("wells_running/P4_creek_current.csv") %>% 
+  mutate(
+    Date.Time = format(
+      as.POSIXct(Date.Time, format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC"),
+      "%Y-%m-%d %H:%M:%S"
+    )
+  ) %>%
+  rename(`Date-Time` = Date.Time)%>%
+  mutate(`Date-Time` = as_datetime(`Date-Time`)) %>%
+  rename(Temperature = Temperature....C)
+P2_running = rbind(p2_new, p4_running)
