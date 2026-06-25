58c7af7f488c7af63cec4dbdbe761ebda972b333
sauron-245 <miners@carleton.edu>
2026-06-25 15:49:04 -0500

    Script for correcting pressure to level is basically online

--- R work/XLE_handling.R
+++ R work/XLE_handling.R
@@ -11,11 +11,16 @@ library(XML)
 ## Function perhaps?
 
 handle_xle = function(location) {
+  
   items_to_pull = c("Date", "Time", "ch1", "ch2") # Set up XML nodes
   result = c("Date", "Time", "Pressure", "Temperature") 
   cols_of_interest = vector("list", length(items_to_pull))
   names(cols_of_interest) = result # Set up column names for final dataframe
 
+ 
+  if (location != "P1_baro") { # only do this if it's a levelogger and not a barologger
+   p1_baro = read_csv("P1_baro_formatted.csv") # 
+  }
   
   path = paste0(location, ".xle")
   xle_file = read_xml(path) # Read in file
@@ -28,9 +33,49 @@ handle_xle = function(location) {
       cols_of_interest[[i]] = cols_of_interest[[i]][2:(length(cols_of_interest[[i]]))]
     }
   }
- df = as.data.frame(cols_of_interest, stringsAsFactors = FALSE) %>% 
-   mutate(Date = ymd(Date)) # Turn vector of lists into dataframe 
- return(df) # Change this 
+  df = as.data.frame(cols_of_interest, stringsAsFactors = FALSE) %>% # Turn vector of lists into dataframe 
+    mutate(Date = ymd(Date), datetime = paste(Date, Time, sep = " "), datetime = ymd_hms(datetime), Temperature = as.numeric(Temperature), Pressure = as.numeric(Pressure)) %>% # merge 'date' and 'time' columns into formatted datetime
+    select(c(-1, -2)) %>% 
+    relocate(datetime) # Clean up
+ if (location != "P1_baro"){ # only do this if it's a levelogger and not a barologger
+   df = df %>%
+    mutate(datetime = round_date(datetime, unit = "15 mins")) %>% # Round time to align with barometric pressure reading
+    left_join(p1_baro %>% select(datetime, Pressure), by = "datetime") %>% # attach pressure data
+    mutate(baro_pressure = (Pressure.y * 0.101972), height_above = (Pressure.x - baro_pressure)) # Convert atmospheric pressure to feet then subtract off
+ }
+ write_csv(df, paste0(location, "_formatted.csv"))
+ 
 }
-handle_xle("P5_8_31")
 
+
+sites = c("P1_baro", "P1", "Bridge", "P5")
+
+for (site in sites) {
+  handle_xle(site)
+}
+
+for (site in sites) {
+  assign(site, read_csv(paste0(site, "_formatted.csv")))
+}
+
+check_csv = function(location) { # Use this to make graphs of temperature over time. Intended to check that dates/expected data gaps are behaving as they ought. 
+  if (location != "P1_baro"){
+  file = read_csv(paste0(location, "_formatted.csv")) 
+  p = file %>% 
+    ggplot(aes(x = datetime, y = height_above)) +
+    geom_line() + 
+    labs(title = location)
+  print(p)
+  }
+}
+for (site in sites){
+  check_csv(site)
+}
+
+bridge_correct = read_csv("BridgeLevellogger.6.23.26.csv") %>% 
+  rename(datetime = DateTime) %>% 
+  mutate(datetime = mdy_hm(datetime)) %>% 
+  rename(height_above = LEVEL)
+ggplot(Bridge, aes(x = datetime, y = height_above)) +
+  geom_line()+
+  geom_line(data = bridge_correct)
