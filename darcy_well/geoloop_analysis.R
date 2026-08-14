a5b5f2a9e6e323181df6beb92d5b182a4b7cd660
sauron-245 <miners@carleton.edu>
2026-08-14 10:12:22 -0500

    friday morning

--- darcy_well/geoloop_analysis.R
+++ darcy_well/geoloop_analysis.R
@@ -32,87 +32,89 @@ geoloop %>%
   # labs(title = "sum bs idk")
 
 ulim = as.POSIXct("2026-07-06 0:00:00")
-llim = as.POSIXct("2026-07-07 0:00:00")
+llim = as.POSIXct("2026-07-08 0:00:00")
 
 P1 = geoloop %>% 
   # filter(date(TIMESTAMP) %in% dates) %>%
   # filter(TIMESTAMP <= ulim1, TIMESTAMP >= llim1) %>%
   ggplot() + 
-  geom_line(aes(x = TIMESTAMP, y = GeoloopToBldgT)) +
+  geom_line(aes(x = TIMESTAMP, y = GeoloopToBldgT), linewidth = 1) +
   theme_bw(base_size = 15) + 
   scale_x_datetime(limits = c(llim, ulim)) +
-  labs(title = "Production Well Return Water Temperature", x = "Date/Time", y = "Water Temp (Deg. C)")
+  labs(title = "Production Well Return Water Temperature", x = "Date", y = "Water Temp (Deg. C)")
 
 P4 = geoloop %>% 
   # filter(date(TIMESTAMP) %in% dates) %>%
   # filter(TIMESTAMP <= ulim1, TIMESTAMP >= llim1) %>%
   ggplot() + 
-  geom_line(aes(x = TIMESTAMP, y = GeoloopGPM)) +
+  geom_line(aes(x = TIMESTAMP, y = GeoloopGPM), linewidth = 1) +
   scale_x_datetime(limits = c(llim, ulim)) +
   theme_bw(base_size = 15) +
-  labs(title = "Geoloop Flow Rate", x = "Date/Time", y = "Flow Rate (GPM")
+  labs(title = "Geoloop Flow Rate", x = "Date", y = "Flow Rate (GPM")
 
 P2 = zentra %>% 
   # filter(date(TIMESTAMP) %in% dates) %>%
   # filter(TIMESTAMP <= ulim1, TIMESTAMP >= llim1) %>%
   ggplot(aes(x = TIMESTAMP, y = water_level_m)) + 
-  geom_line() +
+  geom_line(linewidth = 1) +
   # geom_smooth(method = 'gam', se = FALSE, color = 'blue', formula = y ~ s(x, k = 40, bs = "cs")) +
   scale_x_datetime(limits = c(llim, ulim)) +
   theme_bw(base_size = 15) +
-  labs(title = "Observation Well Water Height", x = "Date/Time", y = "Water Height (M)")
+  labs(title = "Observation Well Water Height", x = "Date", y = "Water Height (M)")
 
 P3 = zentra %>% 
   # filter(date(TIMESTAMP) %in% dates) %>%
   # filter(TIMESTAMP <= ulim1, TIMESTAMP >= llim1) %>%
   ggplot(aes(x = TIMESTAMP, y  = water_temp)) + 
-  geom_line() +
+  geom_line(linewidth = 1) +
   # geom_smooth(method = 'gam', se = FALSE, color = 'blue', formula = y ~ s(x, k = 40, bs = "cs")) +
   scale_x_datetime(limits = c(llim, ulim)) +
   theme_bw(base_size = 15) +
-  labs(title = "Observation Well Water Temperature", x = "Date/Time", y = "Water Temp (Deg. C)")
+  labs(title = "Observation Well Water Temperature", x = "Date", y = "Water Temp (Deg. C)")
 
 P7 = zentra %>% 
   # filter(date(TIMESTAMP) %in% dates) %>%
   # filter(TIMESTAMP <= ulim1, TIMESTAMP >= llim1) %>%
   ggplot(aes(x = TIMESTAMP, y  = spc)) + 
-  geom_line() +
+  geom_line(linewidth = 1) +
   scale_x_datetime(limits = c(llim, ulim)) +
   # geom_smooth(method = 'gam', se = FALSE, color = 'blue', formula = y ~ s(x, k = 40, bs = "cs")) +
   theme_bw(base_size = 15) +
-  labs(title = "Observation Well Specific Conductivity", x = "Date/Time", y = "SPC (Us/cm)")
+  labs(title = "Observation Well Specific Conductivity", x = "Date", y = "SPC (Us/cm)")
 
 parish_jul6 = ggplot() + 
-  geom_line(data = parish %>% filter(depth_m == 22.622), aes(x = dt_cst, y = TMP, color = 'Depth = 75 ft.')) +
-  geom_line(data = parish %>% filter(depth_m == 38.128), aes(x = dt_cst, y = TMP, color = 'Depth = 125 ft.')) +
-  geom_line(data = parish %>% filter(depth_m == 53.380), aes(x = dt_cst, y = TMP, color = 'Depth = 175 ft.')) +
-  geom_line(data = parish %>% filter(depth_m == 68.632), aes(x = dt_cst, y = TMP, color = 'Depth = 225 ft.')) +
+  geom_line(data = parish %>% filter(depth_m == 22.622), aes(x = dt_cst, y = TMP, color = 'Depth = 75 ft.'), linewidth = 1) +
+  geom_line(data = parish %>% filter(depth_m == 38.128), aes(x = dt_cst, y = TMP, color = 'Depth = 125 ft.'), linewidth = 1) +
+  geom_line(data = parish %>% filter(depth_m == 53.380), aes(x = dt_cst, y = TMP, color = 'Depth = 175 ft.'), linewidth = 1) +
+  geom_line(data = parish %>% filter(depth_m == 68.632), aes(x = dt_cst, y = TMP, color = 'Depth = 225 ft.'), linewidth = 1) +
   scale_x_datetime(limits = c(llim, ulim)) +
   theme_bw(base_size = 15) + 
-  labs(x = "Date/Time", y = "Water Temp (deg. C)", title = "Obs. Well Temperature (DTS Cable)", color = "Length Along Fiber")
+  scale_color_manual(breaks = c("Depth = 75 ft.", "Depth = 125 ft.", "Depth = 175 ft.", "Depth = 225 ft."), values = c("skyblue", "goldenrod", "dodgerblue4", "tomato3")) +
+  labs(x = "Date", y = "Water Temp (deg. C)", title = "Obs. Well Temperature (DTS Cable)", color = "Length Along Fiber")
 
 parish_jul23 = ggplot() + 
-  geom_line(data = parish %>% filter(depth_m == 22.622), aes(x = dt_cst, y = TMP, color = 'Depth = 75 ft.')) +
-  geom_line(data = parish %>% filter(depth_m == 38.128), aes(x = dt_cst, y = TMP, color = 'Depth = 125 ft.')) +
-  geom_line(data = parish %>% filter(depth_m == 53.380), aes(x = dt_cst, y = TMP, color = 'Depth = 175 ft.')) +
-  geom_line(data = parish %>% filter(depth_m == 68.632), aes(x = dt_cst, y = TMP, color = 'Depth = 225 ft.')) +
+  geom_line(data = parish %>% filter(depth_m == 22.622), aes(x = dt_cst, y = TMP, color = 'Depth = 75 ft.'), linewidth = 1) +
+  geom_line(data = parish %>% filter(depth_m == 38.128), aes(x = dt_cst, y = TMP, color = 'Depth = 125 ft.'), linewidth = 1) +
+  geom_line(data = parish %>% filter(depth_m == 53.380), aes(x = dt_cst, y = TMP, color = 'Depth = 175 ft.'), linewidth = 1) +
+  geom_line(data = parish %>% filter(depth_m == 68.632), aes(x = dt_cst, y = TMP, color = 'Depth = 225 ft.'), linewidth = 1) +
   scale_x_datetime(limits = c(llim, ulim)) +
   theme_bw(base_size = 15) + 
-  labs(x = "Date/Time", y = "Water Temp (deg. C)", title = "Obs. Well Temperature (DTS Cable)", color = "Length Along Fiber")
+  scale_color_manual(breaks = c("Depth = 75 ft.", "Depth = 125 ft.", "Depth = 175 ft.", "Depth = 225 ft."), values = c("skyblue", "goldenrod", "dodgerblue4", "tomato3")) +
+    labs(x = "Date", y = "Water Temp (deg. C)", title = "Obs. Well Temperature (DTS Cable)", color = "Length Along Fiber")
 
 
 P6 = geoloop %>% 
   # filter(date(TIMESTAMP) %in% dates) %>%
   # filter(TIMESTAMP <= ulim1, TIMESTAMP >= llim1) %>%
   ggplot() + 
-  geom_line(aes(x = TIMESTAMP, y = VFDFreq)) +
+  geom_line(aes(x = TIMESTAMP, y = VFDFreq), linewidth = 1) +
   scale_x_datetime(limits = c(llim, ulim)) +
   theme_bw(base_size = 15) +
-  labs(title = "Production Well Pump Frequency (30 = ON)", x = "Date/Time", y = "Signal Frequency")
+  labs(title = "Production Well Pump Frequency", x = "Date", y = "Signal Frequency")
 
-P6 / P1 / P2 / parish_jul6  + plot_layout(axes = "collect")
+P6 / P2 / P3 / parish_jul6  + plot_layout(axes = "collect")
 
- parish_jul6
+parish_jul6
 
 ggplot(zentra, aes(x = water_level_m, y = water_temp, color = TIMESTAMP)) + 
   geom_jitter() + 
@@ -120,13 +122,14 @@ ggplot(zentra, aes(x = water_level_m, y = water_temp, color = TIMESTAMP)) +
   theme_bw()
 
 parish_jun6 = ggplot() + 
-  geom_line(data = parish_pre %>% filter(depth_m == 22.622), aes(x = dt_cst, y = TMP, color = 'Depth = 75 ft.')) +
-  geom_line(data = parish_pre %>% filter(depth_m == 38.128), aes(x = dt_cst, y = TMP, color = 'Depth = 125 ft.')) +
-  geom_line(data = parish_pre %>% filter(depth_m == 53.380), aes(x = dt_cst, y = TMP, color = 'Depth = 175 ft.')) +
-  geom_line(data = parish_pre %>% filter(depth_m == 68.632), aes(x = dt_cst, y = TMP, color = 'Depth = 225 ft.')) +
+  geom_line(data = parish_pre %>% filter(depth_m == 22.622), aes(x = dt_cst, y = TMP, color = 'Depth = 75 ft.'), linewidth = 1) +
+  geom_line(data = parish_pre %>% filter(depth_m == 38.128), aes(x = dt_cst, y = TMP, color = 'Depth = 125 ft.'), linewidth = 1) +
+  geom_line(data = parish_pre %>% filter(depth_m == 53.380), aes(x = dt_cst, y = TMP, color = 'Depth = 175 ft.'), linewidth = 1) +
+  geom_line(data = parish_pre %>% filter(depth_m == 68.632), aes(x = dt_cst, y = TMP, color = 'Depth = 225 ft.'), linewidth = 1) +
   scale_x_datetime(limits = c(llim, ulim)) +
   theme_bw(base_size = 15) + 
-  labs(x = "Date/Time", y = "Water Temp (deg. C)", title = "Obs. Well Temperature (DTS Cable)", color = "Length Along Fiber")
+  scale_color_manual(breaks = c("Depth = 75 ft.", "Depth = 125 ft.", "Depth = 175 ft.", "Depth = 225 ft."), values = c("skyblue", "goldenrod", "dodgerblue4", "tomato3")) +
+  labs(x = "Date", y = "Water Temp (deg. C)", title = "Obs. Well Temperature (DTS Cable)", color = "Length Along Fiber")
 parish_jun6  
 
 
